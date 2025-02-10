47f2accd92c2df22c8b707d66d7a75e30e5f375d
dearguru07 <pangalaguruyadav101@gmail.com>
2025-02-10 13:25:47 +0530

    g

--- src/QuizCreator.js
+++ src/QuizCreator.js
@@ -1,53 +1,3 @@
-// import React, { useState } from "react";
-
-// const QuizCreator = ({ setQuiz }) => {
-//   const [questions, setQuestions] = useState([]);
-//   const [question, setQuestion] = useState("");
-//   const [options, setOptions] = useState(["", "", "", ""]);
-//   const [correct, setCorrect] = useState(0);
-
-//   const Question = () => {
-//     setQuestions([...questions, { question, options, correct }]);
-//     setQuestion("");
-//     setOptions(["", "", "", ""]);
-//   };
-
-//   const createQuiz = () => {
-//     setQuiz(questions);
-//   };
-
-//   return (
-//     <div className="quiz-Container">
-//       <h2>Create Quiz</h2>
-      
-//       <input className="input-style" type="text" placeholder="Write a Question" value={question} onChange={(e) => setQuestion(e.target.value)}/>
-//       {options.map((option, index) => (
-        
-//         <input className="input-style" key={index} type="text" placeholder={`Option ${index + 1}`} value={option} onChange={(e) => {
-//             const newOptions = [...options];
-//             newOptions[index] = e.target.value;
-//             setOptions(newOptions);
-//           }}
-//         />
-//       ))}
-//       <select className="input-style" value={correct} onChange={(e) => setCorrect(Number(e.target.value))}>
-//         {options.map((_, index) => (
-//           <option key={index} value={index}>
-//             Correct Option: {index + 1}
-//           </option>
-//         ))}
-//       </select>
-//       {/* <button onClick={Question}>Add Question</button> */}
-//       <button onClick={Question}><span class="btn2"><span class="spn2">Add Question</span></span></button>
-//       <button onClick={createQuiz}><span class="btn2"><span class="spn2">Start Quiz</span></span></button>
-//       {/* <button onClick={createQuiz}>Start Quiz</button> */}
-//     </div>
-//   );
-// };
-
-// export default QuizCreator;
-
-
 import React, { useState } from "react";
 
 const QuizCreator = ({ setQuiz }) => {
@@ -56,7 +6,7 @@ const QuizCreator = ({ setQuiz }) => {
   const [options, setOptions] = useState(["", "", "", ""]);
   const [correct, setCorrect] = useState(0);
 
-  const addQuestion = () => {
+  const Question = () => {
     setQuestions([...questions, { question, options, correct }]);
     setQuestion("");
     setOptions(["", "", "", ""]);
@@ -69,19 +19,11 @@ const QuizCreator = ({ setQuiz }) => {
   return (
     <div className="quiz-Container">
       <h2>Create Quiz</h2>
-      <input className="input-style"
-        type="text"
-        placeholder="Question"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-      />
+      
+      <input className="input-style" type="text" placeholder="Write a Question" value={question} onChange={(e) => setQuestion(e.target.value)}/>
       {options.map((option, index) => (
-        <input className="input-style"
-          key={index}
-          type="text"
-          placeholder={`Option ${index + 1}`}
-          value={option}
-          onChange={(e) => {
+        
+        <input className="input-style" key={index} type="text" placeholder={`Option ${index + 1}`} value={option} onChange={(e) => {
             const newOptions = [...options];
             newOptions[index] = e.target.value;
             setOptions(newOptions);
@@ -90,15 +32,18 @@ const QuizCreator = ({ setQuiz }) => {
       ))}
       <select className="input-style" value={correct} onChange={(e) => setCorrect(Number(e.target.value))}>
         {options.map((_, index) => (
-          <option  key={index} value={index}>
+          <option key={index} value={index}>
             Correct Option: {index + 1}
           </option>
         ))}
       </select>
-      <button onClick={addQuestion}><span class="btn2"><span class="spn2">Add Question</span></span></button>
+      {/* <button onClick={Question}>Add Question</button> */}
+      <button onClick={Question}><span class="btn2"><span class="spn2">Add Question</span></span></button>
       <button onClick={createQuiz}><span class="btn2"><span class="spn2">Start Quiz</span></span></button>
+      {/* <button onClick={createQuiz}>Start Quiz</button> */}
     </div>
   );
 };
 
 export default QuizCreator;
+
--- src/QuizTaker.js
+++ src/QuizTaker.js
@@ -50,53 +50,182 @@
 // export default QuizTaker;
 
 
-import React, { useState, useEffect } from "react";
+Skip to content
+ 
+Search Gists
+Search...
+All gists
+Back to GitHub
+@roshanlabh
+roshanlabh/react-phone-book.js
+Created 4 years ago • Report abuse
+Code
+Revisions
+1
+Stars
+32
+Forks
+14
+Clone this repository at &lt;script src=&quot;https://gist.github.com/roshanlabh/eac12ebf9fa76880699256aefcb8d955.js&quot;&gt;&lt;/script&gt;
+<script src="https://gist.github.com/roshanlabh/eac12ebf9fa76880699256aefcb8d955.js"></script>
+Coderbyte - React Phone Book [solution]
+react-phone-book.js
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const QuizTaker = ({ quiz, setScore }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
-  const [timeLeft, setTimeLeft] = useState(30);
+const style = {
+  table: {
+    borderCollapse: "collapse",
+  },
+  tableCell: {
+    border: "1px solid gray",
+    margin: 0,
+    padding: "5px 10px",
+    width: "max-content",
+    minWidth: "150px",
+  },
+  form: {
+    container: {
+      padding: "20px",
+      border: "1px solid #F0F8FF",
+      borderRadius: "15px",
+      width: "max-content",
+      marginBottom: "40px",
+    },
+    inputs: {
+      marginBottom: "5px",
+    },
+    submitBtn: {
+      marginTop: "10px",
+      padding: "10px 15px",
+      border: "none",
+      backgroundColor: "lightseagreen",
+      fontSize: "14px",
+      borderRadius: "5px",
+    },
+  },
+};
 
-  useEffect(() => {
-    const timer = setInterval(() => setTimeLeft((prev) => Math.max(0, prev - 1)), 1000);
-    if (timeLeft === 0) nextQuestion();
-    return () => clearInterval(timer);
-  }, [timeLeft]);
-
-  const handleAnswer = (answer) => {
-    setUserAnswers([...userAnswers, answer]);
-    nextQuestion();
+function PhoneBookForm(props) {
+  const initContact = {
+    id: null,
+    userFirstname: "Coder",
+    userLastname: "Byte",
+    userPhone: "8885559999",
   };
 
-  const nextQuestion = () => {
-    if (currentIndex < quiz.length - 1) {
-      setCurrentIndex(currentIndex + 1);
-      setTimeLeft(30);
-    } else {
-      calculateScore();
-    }
+  const [userState, setUserState] = useState(initContact);
+
+  const handleUserChange = (e) => {
+    setUserState({
+      ...userState,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const calculateScore = () => {
-    const score = userAnswers.reduce(
-      (total, answer, index) => (answer === quiz[index].correct ? total + 1 : total),
-      0
-    );
-    setScore(score);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!userState.userFirstname || !userState.userLastname || !userState.userPhone) return;
+    props.addUser(userState);
+    setUserState(initContact);
   };
 
   return (
-    <div className="quiz-taker">
-      <h2>Take Quiz</h2>
-      <p>Time Left: {timeLeft}s</p>
-      <h3>{quiz[currentIndex].question}</h3>
-      {quiz[currentIndex].options.map((option, index) => (
-        <button key={index} onClick={() => handleAnswer(index)}>
-          {option}
-        </button>
-      ))}
-    </div>
+    <form onSubmit={handleSubmit} style={style.form.container}>
+      <label>First name:</label>
+      <br />
+      <input style={style.form.inputs} className="userFirstname" name="userFirstname" type="text" value={userState.userFirstname} onChange={handleUserChange} />
+      <br />
+      <label>Last name:</label>
+      <br />
+      <input style={style.form.inputs} className="userLastname" name="userLastname" type="text" value={userState.userLastname} onChange={handleUserChange} />
+      <br />
+      <label>Phone:</label>
+      <br />
+      <input style={style.form.inputs} className="userPhone" name="userPhone" type="text" value={userState.userPhone} onChange={handleUserChange} />
+      <br />
+      <input style={style.form.submitBtn} className="submitButton" type="submit" value="Add User" />
+    </form>
   );
-};
+}
 
-export default QuizTaker;
+function InformationTable(props) {
+  const sortedContacts = props.users.sort((a, b) => a.userLastname.localeCompare(b.userLastname));
+
+  const display =
+    sortedContacts.length > 0 ? (
+      sortedContacts.map((user, index) => (
+        <tr key={index}>
+          <td style={style.tableCell}>{user.userFirstname}</td>
+          <td style={style.tableCell}>{user.userLastname}</td>
+          <td style={style.tableCell}>{user.userPhone}</td>
+        </tr>
+      ))
+    ) : (
+      <tr>
+        <td colSpan={3}>&nbsp;</td>
+      </tr>
+    );
+
+  return (
+    <table style={style.table} className="informationTable">
+      <thead>
+        <tr>
+          <th style={style.tableCell}>First name</th>
+          <th style={style.tableCell}>Last name</th>
+          <th style={style.tableCell}>Phone</th>
+        </tr>
+      </thead>
+      <tbody>{display}</tbody>
+    </table>
+  );
+}
+
+function Application(props) {
+  const usersObj = [];
+
+  const [users, setUsers] = useState(usersObj);
+
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
+
+  return (
+    <section>
+      <PhoneBookForm addUser={addUser} />
+      <InformationTable users={users} />
+    </section>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("root"));
+@prestonbourne
+prestonbourne commented on Jun 13, 2022
+I had a code byte assessment with this just yesterday, as soon as I finished I saw someone do it in ten minutes. I on the other hand...took 5 hours.
+
+@gfox1984
+gfox1984 commented on Jul 27, 2022
+Thanks for this gist, it makes it very easy for me to see which of my candidates are cheating :D
+
+@yeuditspiner
+yeuditspiner commented on Feb 15, 2023
+thanks about the solution,
+can you put here the question?
+
+@dearguru07
+Comment
+ 
+Leave a comment
+ 
+Footer
+© 2025 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact
+Manage cookies
+Do not share my personal information
